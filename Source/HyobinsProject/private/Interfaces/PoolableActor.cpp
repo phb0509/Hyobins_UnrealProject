c1b8b1d59083b94b0d9aacfa4ce5917a1937fa10
Hyobin Park <67337620+phb0509@users.noreply.github.com>
2025-02-26 09:59:49 +0900

    인터페이스전용 폴더로 이동.

--- Source/HyobinsProject/private/Interfaces/PoolableActor.cpp
+++ Source/HyobinsProject/private/Interfaces/PoolableActor.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "ActorPool/PoolableActor.h"
+#include "Interfaces/PoolableActor.h"
 
 // Add default functionality here for any IPoolableActor functions that are not pure virtual.
 
