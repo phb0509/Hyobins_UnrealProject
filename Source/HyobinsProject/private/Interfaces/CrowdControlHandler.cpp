2d694b2bce83c6d2661ccbf480982c317c31f6ec
Hyobin Park <67337620+phb0509@users.noreply.github.com>
2025-07-06 14:53:23 +0900

    피격시스템 좀 더 유연하고 확장가능한, 객체지향적으로 다시 설계하기위해 CrowdControl 판정을 할 수 있는 객체를 위한 인터페이스와 컴포넌트 추가.

---
+++ Source/HyobinsProject/private/Interfaces/CrowdControlHandler.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/CrowdControlHandler.h"
+
+// Add default functionality here for any ICrowdControlHandler functions that are not pure virtual.
